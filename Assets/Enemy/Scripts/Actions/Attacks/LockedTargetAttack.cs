72416790211af6ec5d5cb1591640760b3514e988
BiggyBruhmanz <93736992+BiggyBruhmanz@users.noreply.github.com>
2024-08-20 01:07:20 -0400

    adding things

---
+++ Assets/Enemy/Scripts/Actions/Attacks/LockedTargetAttack.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+
+public class SavedTargetFire : EnemyAttack
+{
+    [SerializeField, Tooltip("Number Of Projectiles Per Wave")] private int projectileAmount = 3;
+    [SerializeField, Tooltip("The number of degrees each projectiles are from each other")] private float projectileSpread = 0.1f;
+    [SerializeField, Tooltip("Delay of entire attack. Delay after all waves in seconds")] private float endingDelay = 1f;
+    [SerializeField, Tooltip("Delay between each projectile in seconds")] private float projectileDelay = 0.1f;
+    [SerializeField, Tooltip("Delay between next wave in seconds")] private float waveDelay = 0f;
+    [SerializeField, Tooltip("Number of waves. 1 wave shoots the number of projectiles defined")] private float numberOfWaves = 5f;
+    [SerializeField, Tooltip("Upper value for the random direction a projectile will fire in degrees")] private float maxRandomDir = 5f;
+    [SerializeField, Tooltip("Lower value for the random direction a projectile will fire in degrees")] private float minRandonDir = -5f;
+    //[SerializeField, Tooltip("Amount the projectile is fired infront of player in unity units")] private float predictiveAmount = 0f;
+    [SerializeField, Tooltip("Random distance projectiles could spawn originating from enemy in unity units")] private float spawnOffset = 0f;
+
+    private Vector3 savedTarget;
+
+    public override IEnumerator ExecuteAction(Player player)
+    {
+        if (savedTarget == null)
+            savedTarget = player.transform.position;
+
+        float projectileSpreadRad = projectileSpread * Mathf.Deg2Rad;
+
+        for (int j = 0; j < numberOfWaves; j++) 
+        {
+            for (int i =  -getFloorValue((float)projectileAmount / 2); i <= Mathf.Max(0, getCeilValue((float)projectileAmount / 2) - 1); i++)
+            {
+                Vector3 targetDir = savedTarget - transform.position;// + player.PlayerMovement.GetVelocity() * predictiveAmount;
+                float angle = Mathf.Atan2(targetDir.y, targetDir.x) + Random.Range(minRandonDir * Mathf.Deg2Rad, maxRandomDir * Mathf.Deg2Rad);
+
+                float evenProjectileAdjust = projectileAmount % 2 == 0 ? projectileSpreadRad / 2 : 0;
+                SpawnProjectile(angle + i * projectileSpreadRad + evenProjectileAdjust, projectilePrefab1, spawnOffset);
+
+                if (projectileDelay != 0)
+                    yield return new WaitForSeconds(projectileDelay);
+            }
+
+            if (waveDelay != 0)
+                yield return new WaitForSeconds(waveDelay);
+        }
+
+        if (endingDelay != 0)
+            yield return new WaitForSeconds(endingDelay);
+    }
+
+    private int getFloorValue(float input)
+    {
+        return (int) Mathf.Floor(input);
+    }
+
+    private int getCeilValue(float input)
+    {
+        return (int)Mathf.Ceil(input);
+    }
+}
